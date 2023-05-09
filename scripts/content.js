78905267d4fc16170a5e0ab1b358c8b43c880d44
Emre Cenk <emrecenk9@gmail.com>
2023-05-08 23:46:30 -0400

    tried something'

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,13 @@
 
 
 console.log("WE ARE HERE RIGHT FMLFMLFML")
+
+const collumheaders = document.getElementsByTagName("th");
+let orgCollumIndex = 3;
+for (let i = 0; i < collumheaders.length; i++){
+    if ( collumheaders[i].innerText.toLowerCase().includes( "org" ) ) {
+        orgCollumIndex = i;
+        break;
+    }
+}
+console.log("INDEX:", orgCollumIndex)
